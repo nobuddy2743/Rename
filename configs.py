cc5c353ff57fbf102633958e24b457aae7abfda3
Black candy <122829729+blackcandy-54@users.noreply.github.com>
2023-05-01 10:00:30 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -14,12 +14,12 @@ logging.basicConfig(
 class Config(object):
     API_ID = int(os.environ.get("API_ID", "21979478"))
     API_HASH = os.environ.get("API_HASH", "cb446955477336637eeb6ecb0a0d0a11")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6125888099:AAEwsKbyXEJiENCKQAXMPxlFyu4u9HI6zpE")
     DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "./downloads")
     LOGGER = logging
     OWNER_ID = int(os.environ.get("OWNER_ID", "1345158291 1767901454"))
     PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "0").split()))
     PRO_USERS.append(OWNER_ID)
-    MONGODB_URI = os.environ.get("MONGODB_URI", "")
+    MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://bala1692:bala1692@cluster0.fspdlv5.mongodb.net/?retryWrites=true&w=majority")
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001678508201"))
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
