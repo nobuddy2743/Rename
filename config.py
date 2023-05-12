3c315a4da8df453edb852975b4b98f5be288bcdd
Black candy <122829729+blackcandy-54@users.noreply.github.com>
2023-05-12 10:27:57 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+from os import environ
+
+API_ID = int(environ.get("API_ID", "21979478"))
+API_HASH = environ.get("API_HASH", "cb446955477336637eeb6ecb0a0d0a11")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6125888099:AAEwsKbyXEJiENCKQAXMPxlFyu4u9HI6zpE")
+ADMIN = int(environ.get("ADMIN", "1345158291"))          
+CAPTION = environ.get("CAPTION", "")
+GROUP_ID = int(environ.get("GROUP_ID", "-1001446318836")) 
+
+# for thumbnail ( back end is MrMKN brain 😉)
+DOWNLOAD_LOCATION = "./DOWNLOADS"
+
