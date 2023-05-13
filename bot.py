c02eaed4e73458bfe3f4514f7f797e9fcf3d48f3
Black candy <122829729+blackcandy-54@users.noreply.github.com>
2023-05-13 09:12:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,18 +4,18 @@ import os
 
 
 plugins = dict( root="plugins")
-    Bot = Client(
+    app1 = Client(
         "AnyDLBot",
         bot_token=,
         api_id=APP_ID,
         api_hash=API_HASH,
         plugins=plugins)
 
-    app2 = pyrogram.Client(
+    app2 = Client(
         "Userbot",
         api_id=APP_ID,
         api_hash=API_HASH,
-        user_session="")
+        user_session="BABKlyIAshAUqAQUvB2Tu8e6itHrZzPdHUY3kBpUowO7ZqDpEZr5w6y3F4K4IrY9Zcfa3se5ubn85gUmv5STvxIlivAI83Kz3mtgVWaan7zNYzVDt8MoeUVH3nXXBH5GeaL6dWmh9ZmvdZ_3mvJgEuBxHBZ4L1Bpm3kuhnTqNERIztUZGcyTGhzaCFxRie1MEYLCe_qAq-llf_vF7MTTwf2jS0zWT9FPw6uhzQTOQRyKxxDgrrlr5Sk2NRv-69qFYGVwhMWkjCoEVBnaE4JllgosYIbi-DJ9BRgEngPs2eiUV_6YdqWcp7S8wbcK7XOtyuNMnh6JdiepKdzLyO-eGAsGnG3JUgAAAABpYAkOAA")
 
 
     
