37b68eb592e32a6237ddb884a854dd9e39781f5b
Black candy <122829729+blackcandy-54@users.noreply.github.com>
2023-05-13 09:10:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,20 +13,15 @@ plugins = dict( root="plugins")
 
     app2 = pyrogram.Client(
         "Userbot",
-        api_id=Config.APP_ID,
-        api_hash=Config.API_HASH,
-        user_session="hdjsjd")
+        api_id=APP_ID,
+        api_hash=API_HASH,
+        user_session="")
 
 
     
 
     
-    user = Client(
-        "Userbot",
-        api_id="APP_ID",
-        api_hash="API_HASH",
-        user_session="BABKlyIAshAUqAQUvB2Tu8e6itHrZzPdHUY3kBpUowO7ZqDpEZr5w6y3F4K4IrY9Zcfa3se5ubn85gUmv5STvxIlivAI83Kz3mtgVWaan7zNYzVDt8MoeUVH3nXXBH5GeaL6dWmh9ZmvdZ_3mvJgEuBxHBZ4L1Bpm3kuhnTqNERIztUZGcyTGhzaCFxRie1MEYLCe_qAq-llf_vF7MTTwf2jS0zWT9FPw6uhzQTOQRyKxxDgrrlr5Sk2NRv-69qFYGVwhMWkjCoEVBnaE4JllgosYIbi-DJ9BRgEngPs2eiUV_6YdqWcp7S8wbcK7XOtyuNMnh6JdiepKdzLyO-eGAsGnG3JUgAAAABpYAkOAA")
-       
-bot = Bot()
+
+
 bot.run()
 user.run()
