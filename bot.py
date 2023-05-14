a9815cda2dbd0697c7515d49bdc26b8446f3ae1a
C͡linton Abraꫝam <70835540+Clinton-Abraham@users.noreply.github.com>
2023-05-14 15:51:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ForceRepl
 
 from main.utils import progress_message, humanbytes
 
-plugins = dict( root="main")
+
 User_Session = "BABKlyIAshAUqAQUvB2Tu8e6itHrZzPdHUY3kBpUowO7ZqDpEZr5w6y3F4K4IrY9Zcfa3se5ubn85gUmv5STvxIlivAI83Kz3mtgVWaan7zNYzVDt8MoeUVH3nXXBH5GeaL6dWmh9ZmvdZ_3mvJgEuBxHBZ4L1Bpm3kuhnTqNERIztUZGcyTGhzaCFxRie1MEYLCe_qAq-llf_vF7MTTwf2jS0zWT9FPw6uhzQTOQRyKxxDgrrlr5Sk2NRv-69qFYGVwhMWkjCoEVBnaE4JllgosYIbi-DJ9BRgEngPs2eiUV_6YdqWcp7S8wbcK7XOtyuNMnh6JdiepKdzLyO-eGAsGnG3JUgAAAABpYAkOAA"
 
 
@@ -15,8 +15,7 @@ app1 = Client(
         "AnyDLBot",
         bot_token=BOT_TOKEN,
         api_id=API_ID,
-        api_hash=API_HASH,
-        plugins=plugins)
+        api_hash=API_HASH)
         
 
 app2 = Client(
@@ -163,9 +162,6 @@ async def refunc(bot, msg):
         await msg.delete()
         await reply_message.delete()
     
-
-    
-print("i")
 
 app1.start()
 app2.start()
