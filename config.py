98920d61a954c5116952d538a7c0c387965fe1cc
C͡linton Abraꫝam <70835540+Clinton-Abraham@users.noreply.github.com>
2023-05-17 16:41:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
-from os import environ
 
-API_ID = int(environ.get("API_ID", "21979478"))
-API_HASH = environ.get("API_HASH", "cb446955477336637eeb6ecb0a0d0a11")
-BOT_TOKEN = environ.get("BOT_TOKEN", "6125888099:AAEwsKbyXEJiENCKQAXMPxlFyu4u9HI6zpE")
-ADMIN = int(environ.get("ADMIN", "1345158291"))          
-CAPTION = environ.get("CAPTION", "")
-GROUP_ID = int(environ.get("GROUP_ID", "-1001446318836")) 
-USER_SESSION = environ.get("USER_SESSION", "BABKlyIAshAUqAQUvB2Tu8e6itHrZzPdHUY3kBpUowO7ZqDpEZr5w6y3F4K4IrY9Zcfa3se5ubn85gUmv5STvxIlivAI83Kz3mtgVWaan7zNYzVDt8MoeUVH3nXXBH5GeaL6dWmh9ZmvdZ_3mvJgEuBxHBZ4L1Bpm3kuhnTqNERIztUZGcyTGhzaCFxRie1MEYLCe_qAq-llf_vF7MTTwf2jS0zWT9FPw6uhzQTOQRyKxxDgrrlr5Sk2NRv-69qFYGVwhMWkjCoEVBnaE4JllgosYIbi-DJ9BRgEngPs2eiUV_6YdqWcp7S8wbcK7XOtyuNMnh6JdiepKdzLyO-eGAsGnG3JUgAAAABpYAkOAA")
-# for thumbnail ( back end is MrMKN brain 😉)
-DOWNLOAD_LOCATION = "./DOWNLOADS"
+class Config(object):
+
+API_ID = int("21979478")
+API_HASH = str("cb446955477336637eeb6ecb0a0d0a11")
+BOT_TOKEN = str("6125888099:AAEwsKbyXEJiENCKQAXMPxlFyu4u9HI6zpE")
+GROUP_ID = int("-1001446318836")
+USER_SESSION = "BABKlyIAshAUqAQUvB2Tu8e6itHrZzPdHUY3kBpUowO7ZqDpEZr5w6y3F4K4IrY9Zcfa3se5ubn85gUmv5STvxIlivAI83Kz3mtgVWaan7zNYzVDt8MoeUVH3nXXBH5GeaL6dWmh9ZmvdZ_3mvJgEuBxHBZ4L1Bpm3kuhnTqNERIztUZGcyTGhzaCFxRie1MEYLCe_qAq-llf_vF7MTTwf2jS0zWT9FPw6uhzQTOQRyKxxDgrrlr5Sk2NRv-69qFYGVwhMWkjCoEVBnaE4JllgosYIbi-DJ9BRgEngPs2eiUV_6YdqWcp7S8wbcK7XOtyuNMnh6JdiepKdzLyO-eGAsGnG3JUgAAAABpYAkOAA")
+DOWNLOADS = str("./DOWNLOADS")
 
