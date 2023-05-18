1585f16a1a3abbb3975ca72f0cf00a99dc34805b
C͡linton Abraꫝam <70835540+Clinton-Abraham@users.noreply.github.com>
2023-05-18 10:16:08 +0530

    Update module01.py

--- main/module01.py
+++ main/module01.py
@@ -1,1 +1,15 @@
+import time
 
+async def Xownload(bot, update, imog, download_location)
+    try:
+        ctime = time.time()
+        file_location = await bot.download_media(
+        message=update.reply_to_message,
+        file_name=download_location,
+        progress=progress_message,
+        progress_args=("Downloading...", imog, ctime))
+    except Exception as e:
+        await imog.edit(text=f"ERROR : {e}")
+        file_location = None
+
+    return file_location
