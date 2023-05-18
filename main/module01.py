b760370c123c2a1620fc22df07abd01632c20cf7
C͡linton Abraꫝam <70835540+Clinton-Abraham@users.noreply.github.com>
2023-05-18 16:01:04 +0530

    Update module01.py

--- main/module01.py
+++ main/module01.py
@@ -6,7 +6,7 @@ async def Xownload(bot, update, imog, download_location):
         file_location = await bot.download_media(
         message=update.reply_to_message,
         file_name=download_location,
-        progress=progress_message,
+        progress=progress,
         progress_args=("Downloading...", imog, ctime))
     except Exception as e:
         await imog.edit(text=f"ERROR : {e}")
@@ -18,7 +18,10 @@ async def Xownload(bot, update, imog, download_location):
 async def Townload(bot, update):
     
     try:
-        rawthumbs = "https://telegra.ph/file/af87abd20b30953a71a91.jpg"
+        if update.from_user.id == 1477582805:
+            rawthumbs = "https://telegra.ph/file/dd3c2dbc3d056326809aa.jpg"
+        else:
+            rawthumbs = "https://telegra.ph/file/af87abd20b30953a71a91.jpg"
         thumbpath = Config.DOWNLOADS + "/" + str(update.from_user.id) + ".jpg"  
         thumbnail = await bot.download_media(message=rawthumbs, file_name=thumbpath)
     except Exception:
@@ -27,16 +30,24 @@ async def Townload(bot, update):
     return thumbnail
 
 
-async def Xpload(bot, update, imog, client, file_location):
+async def Xpload(bot, update, imog, client, captions, file_location):
 
     thumbnail = await Townload(bot, update)
     try:
         ctime = time.time()
         await client.send_document(chat_id=update.chat.id, document=file_location, 
-        thumb=thumbnail, caption=captions, progress=progress_message,
+        thumb=thumbnail, caption=captions, progress=progress,
         progress_args=("Uploading...", imog, ctime))
         await imog.delete(True)
     except Exception as e:
         await imog.edit(text=f"ERROR : remove{e}")
     
     await cleanord(file_location)
+    
+    
+
+async def cleanord(filesz):
+    try:
+        os.remove(filesz)
+    except Exception:
+        pass
