cc4dbd45c11b6b1e9354a64a5bb08eb8a4d3221f
Black candy <122829729+blackcandy-54@users.noreply.github.com>
2023-05-26 23:23:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,4 +15,4 @@ class Config(object):
 
     USESSION = str("BABKlyIAshAUqAQUvB2Tu8e6itHrZzPdHUY3kBpUowO7ZqDpEZr5w6y3F4K4IrY9Zcfa3se5ubn85gUmv5STvxIlivAI83Kz3mtgVWaan7zNYzVDt8MoeUVH3nXXBH5GeaL6dWmh9ZmvdZ_3mvJgEuBxHBZ4L1Bpm3kuhnTqNERIztUZGcyTGhzaCFxRie1MEYLCe_qAq-llf_vF7MTTwf2jS0zWT9FPw6uhzQTOQRyKxxDgrrlr5Sk2NRv-69qFYGVwhMWkjCoEVBnaE4JllgosYIbi-DJ9BRgEngPs2eiUV_6YdqWcp7S8wbcK7XOtyuNMnh6JdiepKdzLyO-eGAsGnG3JUgAAAABpYAkOAA")
 
-    CAPTIONS = Str("Join @SingleMachiOffl\n\nOther Channels (files\nTamil -> @SingleMachiCinemas\nMalayalam -> @SingleMachiMallu\nWebSeries -> @SingleMachiSerie\nHindi -> @SingleMachiBollyWood\nHollywood -> @SingleXMachi\nAnime -> @SingleMachiAnime
+    CAPTIONS = Str("Join @SingleMachiOffl\n\nOther Channels files:\nTamil -> @SingleMachiCinemas\nMalayalam -> @SingleMachiMallu\nWebSeries -> @SingleMachiSerie\nHindi -> @SingleMachiBollyWood\nHollywood -> @SingleXMachi\nAnime -> @SingleMachiAnime")
